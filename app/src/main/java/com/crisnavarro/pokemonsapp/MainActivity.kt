b183339c38d468a505036ed397c474a444ee087c
Cristian Navarro Gonzalez <cristianskate82@hotmail.com>
2022-12-15 15:31:36 -0600

    first commit

---
+++ app/src/main/java/com/crisnavarro/pokemonsapp/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.crisnavarro.pokemonsapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.crisnavarro.pokemonsapp.ui.theme.PokemonsAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PokemonsAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = "") {
+                        composable("") {}
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/crisnavarro/pokemonsapp/PokemonApplication.kt
@@ -0,0 +1,8 @@
+package com.crisnavarro.pokemonsapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PokemonApplication : Application() {
+}
